a0f34a18acfffd16056a90b03d915cfef4c7f4b6
lifetruman <oneslifetruman@gmail.com>
2022-04-22 00:05:16 +0900

    feat: home (home-description-2,3 img&txt) fadeIn javascript 추가 (#11)

---
+++ front/js/fadeIn.js
@@ -0,0 +1,21 @@
+// Scroll Animation (sa, 스크롤 애니메이션)
+const saTriggerMargin = 300;
+const saElementList = document.querySelectorAll(
+  ".home-img--fadeIn,.home-txt--fadeIn"
+);
+
+const saFunc = function () {
+  for (const element of saElementList) {
+    if (!element.classList.contains("show")) {
+      if (
+        window.innerHeight >
+        element.getBoundingClientRect().top + saTriggerMargin
+      ) {
+        element.classList.add("show");
+      }
+    }
+  }
+};
+
+window.addEventListener("load", saFunc);
+window.addEventListener("scroll", saFunc);
