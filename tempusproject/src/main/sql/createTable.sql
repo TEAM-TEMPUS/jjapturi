379dea8b1fc29334859838bd9ae3c8f41f9f6643
lifetruman <oneslifetruman@gmail.com>
2022-04-27 11:46:18 +0900

    docs: Member, Member_Image 테이블 생성 sql문 작성

--- tempusproject/src/main/sql/createTable.sql
+++ tempusproject/src/main/sql/createTable.sql
@@ -26,3 +26,22 @@ grade TINYINT,
 CONSTRAINT Trading_Info_FK_member_id FOREIGN KEY (member_id) REFERENCES Member(member_id),
 CONSTRAINT Trading_Info_FK_product_id FOREIGN KEY (product_id) REFERENCES Product(product_id)
 );
+
+create table Member(member_id BIGINT auto_increment primary key,
+id VARCHAR(25),
+nickname VARCHAR(25),
+password VARCHAR(25),
+self_introduction VARCHAR(2000),
+address VARCHAR(50),
+email VARCHAR(40),
+phone_number VARCHAR(20),
+constraint Member_UK_id UNIQUE (id),
+constraint Member_UK_nickname UNIQUE (nickname)
+);
+
+create table Member_Image(member_image_id BIGINT auto_increment primary key,
+member_id BIGINT,
+store_img_name VARCHAR(50),
+constraint Member_Image_FK_member_id FOREIGN KEY(member_id)REFERENCES Member(member_id) ON DELETE CASCADE,
+constraint Member_Image_UK_store_img_name UNIQUE (store_img_name)
+);
