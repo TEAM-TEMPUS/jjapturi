14caad2a5225e6fdd6f5a02ed36a279c7e66f455
JunYoung-C <doforme524@gmail.com>
2022-04-28 21:31:24 +0900

    feat: TradingInfoDao에서 완료된 거래 등급 목록을 조회하는 메소드의 sql문을 inner 조인으로 변경 (#57)

--- tempusproject/src/main/java/dao/TradingInfoDao.java
+++ tempusproject/src/main/java/dao/TradingInfoDao.java
@@ -15,7 +15,10 @@ public class TradingInfoDao {
 	public List<String> findCompleteTrandingsByMemberId(Long memberId) {
 		List<String> gradeList = new ArrayList<>();
 		DbConnect db = new DbConnect();		
-		String sql = "select ti.grade from Trading_Info ti, Service s where ti.member_id = ? and s.status = 'completion'";
+		String sql = "select ti.grade from Trading_Info ti "
+				+ "INNER JOIN Service s "
+				+ "on ti.member_id = s.member_id and s.status = 'completion' "
+				+ "where ti.member_id = ?";
 		Connection conn = db.getConnection();
 		PreparedStatement ps = null;
 		ResultSet rs = null;
