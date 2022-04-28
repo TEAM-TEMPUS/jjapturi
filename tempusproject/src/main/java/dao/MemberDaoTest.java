7d2ddc64f5675ebe9abf3987496f21b9b4e2eefe
JunYoung-C <doforme524@gmail.com>
2022-04-28 11:50:52 +0900

    test: MemberDao 회원 조회 기능 테스트

--- tempusproject/src/main/java/dao/MemberDaoTest.java
+++ tempusproject/src/main/java/dao/MemberDaoTest.java
@@ -8,7 +8,8 @@ public class MemberDaoTest {
 	public static void main(String[] args) {
 		MemberDaoTest test = new MemberDaoTest();
 //		test.save();
-		test.update();
+//		test.update();
+		test.findByMemberId(1L);
 		
 	}
 
@@ -20,5 +21,10 @@ public class MemberDaoTest {
 	public void update() {
 		MemberDto dto = new MemberDto("nickname", "chlwnsdud111", "password", "address", "email", "phoneNumber");
 		dao.update(dto);
+	}
+	
+	public void findByMemberId(Long memberId) {
+		MemberDto member = dao.findByMemberId(memberId);
+		System.out.println(member);
 	}
 }
