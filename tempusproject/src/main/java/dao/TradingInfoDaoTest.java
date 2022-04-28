23265e14c3432d263b0a339e24b60285dec63cf9
JunYoung-C <doforme524@gmail.com>
2022-04-28 21:31:39 +0900

    test: 변경된 sql문 작동 테스트

--- tempusproject/src/main/java/dao/TradingInfoDaoTest.java
+++ tempusproject/src/main/java/dao/TradingInfoDaoTest.java
@@ -1,24 +1,16 @@
 package dao;
 
-import java.sql.Connection;
-import java.sql.PreparedStatement;
-import java.sql.ResultSet;
-import java.sql.SQLException;
-import java.util.ArrayList;
 import java.util.List;
-
-import dto.MemberDto;
-import mysql.db.DbConnect;
 
 public class TradingInfoDaoTest {
 	static TradingInfoDao dao = new TradingInfoDao();
 	
 	public static void main(String[] args) {
 		TradingInfoDaoTest test = new TradingInfoDaoTest();
-		test.findByMemberId(1L);
+		test.findCompleteTrandingsByMemberId(1L);
 	}
 	
-	public void findByMemberId(Long memberId) {
+	public void findCompleteTrandingsByMemberId(Long memberId) {
 		List<String> gradeList = dao.findCompleteTrandingsByMemberId(memberId);
 		for (String grade : gradeList) {
 			System.out.println(grade);
