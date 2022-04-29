cef8cee349366f9d486eb1e7b331d3b1005a4321
JunYoung-C <doforme524@gmail.com>
2022-04-29 11:39:23 +0900

    feat: TradingInfoDto 생성 (#57)

---
+++ tempusproject/src/main/java/dto/TradingInfoDto.java
@@ -0,0 +1,18 @@
+package dto;
+
+public class TradingInfoDto {
+	private String grade;
+
+	public TradingInfoDto(String grade) {
+		super();
+		this.grade = grade;
+	}
+
+	public String getGrade() {
+		return grade;
+	}
+
+	public void setGrade(String grade) {
+		this.grade = grade;
+	}
+}
