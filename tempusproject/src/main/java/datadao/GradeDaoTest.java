cc54fd035a927a9a8de544bced30abbebd6cab65
yongki1403 <dydrlwja55@naver.com>
2022-04-30 19:24:46 +0900

    test: GradeDao의 거래 평점을 등록하는 기능 테스트

---
+++ tempusproject/src/main/java/datadao/GradeDaoTest.java
@@ -0,0 +1,17 @@
+package datadao;
+
+import datadto.GradeDto;
+
+public class GradeDaoTest {
+	static GradeDao dao=new GradeDao();
+	
+	public static void main(String[] args) {
+		GradeDaoTest test=new GradeDaoTest();		
+		test.insertGrade();
+		
+	}
+	public void insertGrade() {
+		GradeDto dto=new GradeDto(1L,1L,1L,3);
+		dao.insertGrade(dto);
+	}
+}
