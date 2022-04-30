655f0c54fd4a16fa08ebf54b1862d4db9a7ae70e
yongki1403 <dydrlwja55@naver.com>
2022-04-30 19:14:04 +0900

    feat: ServiceImageDao에 서비스 수정 페이지 이미지를 조회하는 기능 추가 (#71)

---
+++ tempusproject/src/main/java/datadao/ServiceImageDao.java
@@ -0,0 +1,50 @@
+package datadao;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+
+import datadto.ServiceImgDto;
+import mysql.db.DbConnect;
+
+public class ServiceImageDao {
+	DbConnect db=new DbConnect();
+	
+	public List<ServiceImgDto> findByServiceImage(Long serviceId){
+		List<ServiceImgDto> list=new ArrayList<>();
+		DbConnect db=new DbConnect();
+		
+		Connection conn=db.getConnection();
+		PreparedStatement pstmt=null;
+		ResultSet rs=null;
+		
+		String sql="select * from Service_Image where service_id=?";
+		
+		try {
+			pstmt=conn.prepareStatement(sql);
+			pstmt.setLong(1, serviceId);
+			rs=pstmt.executeQuery();
+			
+			while(rs.next()) {
+				ServiceImgDto dto=new ServiceImgDto();
+				dto.setServiceImageId(rs.getLong("service_image_id"));
+				dto.setServiceId(rs.getLong("service_id"));
+				dto.setStoreImgName(rs.getString("store_img_name"));
+			
+				list.add(dto);
+			}
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}finally {
+			db.dbClose(rs, pstmt, conn);
+		}
+		
+		return list;
+		
+		
+	}
+}
