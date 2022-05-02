23802a2f14326094904f9f48ff6f30106bbbbaeb
JunYoung-C <doforme524@gmail.com>
2022-05-02 12:58:45 +0900

    feat: ServiceInqueryDto에 파라미터 생성자 추가

--- tempusproject/src/main/java/data/dto/service/ServiceInqueryDto.java
+++ tempusproject/src/main/java/data/dto/service/ServiceInqueryDto.java
@@ -4,6 +4,8 @@ import java.sql.Date;
 import java.util.List;
 
 public class ServiceInqueryDto {
+	private Long serviceId;
+	private Long memberId;
 	private String types;
 	private String title;
 	private String category;
@@ -12,57 +14,89 @@ public class ServiceInqueryDto {
 	private Date endDate;
 	private int price;
 	private String description;
-	
-	
+
+	public ServiceInqueryDto() {
+	}
+
+	public ServiceInqueryDto(Long serviceId, Long memberId, String types, String title, String category, String place,
+			Date startDate, Date endDate, int price, String description) {
+		super();
+		this.serviceId = serviceId;
+		this.memberId = memberId;
+		this.types = types;
+		this.title = title;
+		this.category = category;
+		this.place = place;
+		this.startDate = startDate;
+		this.endDate = endDate;
+		this.price = price;
+		this.description = description;
+	}
 
 	public String getTypes() {
 		return types;
 	}
+
 	public void setTypes(String types) {
 		this.types = types;
 	}
+
 	public String getTitle() {
 		return title;
 	}
+
 	public void setTitle(String title) {
 		this.title = title;
 	}
+
 	public String getCategory() {
 		return category;
 	}
+
 	public void setCategory(String category) {
 		this.category = category;
 	}
+
 	public String getPlace() {
 		return place;
 	}
+
 	public void setPlace(String place) {
 		this.place = place;
 	}
+
 	public Date getStartDate() {
 		return startDate;
 	}
+
 	public void setStartDate(Date startDate) {
 		this.startDate = startDate;
 	}
+
 	public Date getEndDate() {
 		return endDate;
 	}
+
 	public void setEndDate(Date endDate) {
 		this.endDate = endDate;
 	}
+
 	public int getPrice() {
 		return price;
 	}
+
 	public void setPrice(int price) {
 		this.price = price;
 	}
+
 	public String getDescription() {
 		return description;
 	}
+
 	public void setDescription(String description) {
 		this.description = description;
 	}
+
 	@Override
 	public String toString() {
 		return "ServiceInqueryDto [types=" + types + ", title=" + title + ", category=" + category + ", place=" + place
@@ -70,7 +104,4 @@ public class ServiceInqueryDto {
 				+ description + "]";
 	}
 
-
-	
-	
 }
