904ae757438576ebbe51a21a6e4659f03a2383b3
JunYoung-C <doforme524@gmail.com>
2022-05-02 09:58:09 +0900

    refactor: 중복되는 성격의 파일 병합 (#88)

--- tempusproject/src/main/java/data/dao/service/ServiceImageDao.java
+++ tempusproject/src/main/java/data/dao/service/ServiceImageDao.java
@@ -2,10 +2,14 @@ package data.dao.service;
 
 import java.sql.Connection;
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
 
 import data.dto.service.ServiceDto;
 import data.dto.service.ServiceImageDto;
+import data.dto.service.ServiceImgDto;
 import mysql.db.DbConnect;
 
 public class ServiceImageDao {
@@ -32,5 +36,38 @@ public class ServiceImageDao {
 		} finally {
 			db.dbClose(pstmt, conn);
 		}
+	}
+	
+	public List<ServiceImgDto> findAllByServiceId(Long serviceId){
+		List<ServiceImgDto> list=new ArrayList<>();
+		DbConnect db=new DbConnect();
+		
+		Connection conn=db.getConnection();
+		PreparedStatement pstmt=null;
+		ResultSet rs=null;
+		
+		String sql="select * from Service_Image where service_id=?";
+		
+		try {
+			pstmt=conn.prepareStatement(sql);
+			pstmt.setLong(1, serviceId);
+			rs=pstmt.executeQuery();
+			
+			while(rs.next()) {
+				ServiceImgDto dto=new ServiceImgDto();
+				dto.setServiceImageId(rs.getLong("service_image_id"));
+				dto.setServiceId(rs.getLong("service_id"));
+				dto.setStoreImgName(rs.getString("store_img_name"));
+			
+				list.add(dto);
+			}
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}finally {
+			db.dbClose(rs, pstmt, conn);
+		}
+		
+		return list;
 	}
 }
