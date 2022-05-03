43889c205538800fc84735e870aaa6a7fc9b388f
JunYoung-C <doforme524@gmail.com>
2022-05-03 20:35:23 +0900

    feat: TradingInfoDto에 types 추가

--- tempusproject/src/main/java/data/dto/tradinginfo/TradingInfoDto.java
+++ tempusproject/src/main/java/data/dto/tradinginfo/TradingInfoDto.java
@@ -5,8 +5,7 @@ public class TradingInfoDto {
 	private Long memberId;
 	private Long serviceId;
 	private int grade;
-	
-
+	private String types;
 
 	public TradingInfoDto(Long memberId, Long serviceId, int grade) {
 		super();
@@ -15,7 +14,6 @@ public class TradingInfoDto {
 		this.grade = grade;
 	}
 
-
 	public TradingInfoDto(Long tradingInfoId, Long memberId, Long serviceId, int grade) {
 		super();
 		this.tradingInfoId = tradingInfoId;
@@ -23,33 +21,46 @@ public class TradingInfoDto {
 		this.serviceId = serviceId;
 		this.grade = grade;
 	}
-	
-	
+
+	public TradingInfoDto(Long tradingInfoId, Long memberId, Long serviceId, int grade, String types) {
+		super();
+		this.tradingInfoId = tradingInfoId;
+		this.memberId = memberId;
+		this.serviceId = serviceId;
+		this.grade = grade;
+		this.types = types;
+	}
+
 	public Long getTradingInfoId() {
 		return tradingInfoId;
 	}
+
 	public void setTradingInfoId(Long tradingInfoId) {
 		this.tradingInfoId = tradingInfoId;
 	}
+
 	public Long getMemberId() {
 		return memberId;
 	}
+
 	public void setMemberId(Long memberId) {
 		this.memberId = memberId;
 	}
+
 	public Long getServiceId() {
 		return serviceId;
 	}
+
 	public void setServiceId(Long serviceId) {
 		this.serviceId = serviceId;
 	}
+
 	public int getGrade() {
 		return grade;
 	}
+
 	public void setGrade(int grade) {
 		this.grade = grade;
 	}
 
-
-	
 }
